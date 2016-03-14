0d166c50cf4bf2e2b7afc271d2516178d7afe434
namitn1 <namitn1@users.noreply.github.com>
2016-03-15 00:55:27 +0530

    Updated

---
+++ resultnotifier.py
@@ -0,0 +1,19 @@
+import urllib2
+import re
+import os
+import time
+ 
+while 1:
+ 
+    html_content = urllib2.urlopen('http://www.updates.collegespace.in/category/results').read() 
+ 
+    matches = re.findall('5thSem2015', html_content);  
+ 
+ 
+    if len(matches) == 0: 
+       os.system("notify-send 'Yeah' 'Result is not declared yet'")
+       time.sleep(900)
+ 
+    else:
+       os.system("notify-send 'Oops' 'Result Declared'")
+       quit()
